6cd974430d4574336986c986cfee722e74e08bc8
Niniola Adesina Jamiu <31655138+Apesin@users.noreply.github.com>
2022-02-16 20:21:54 +0100

    Update web3.js

--- js/web3.js
+++ js/web3.js
@@ -1,4 +1,4 @@
-        var myWallet = document.getElementById('myWallet');
+        var myWallet = document.getElementById('w');
 
         async function loadWeb3() {
             if (window.ethereum) {
@@ -9,7 +9,7 @@
 
         async function getCurrentAccount() {
             const accounts = await window.web3.eth.getAccounts();
-//                 myWallet.innerHTML = myWallet.innerHTML+"<h5>"+accounts[0]+"</h5>";
+                myWallet.innerHTML = myWallet.innerHTML+"<h5>"+accounts[0]+"</h5>";
              console.log(accounts[0]);
         }
 
